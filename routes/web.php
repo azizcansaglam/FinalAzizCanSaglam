dde7c86c08948b5752a427dd8f64c4333e6b57fa
root <root@DESKTOP-KAAP53E.localdomain>
2021-06-05 12:19:40 +0300

    Create route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [StudentController::class,'index'])->name('student.index');
+Route::get('/newStudent', [StudentController::class,'create'])->name('student.newtudent');
+Route::post('/save', [StudentController::class,'save'])->name('student.save');
